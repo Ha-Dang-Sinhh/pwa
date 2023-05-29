401f532eddc7974953b1e043e500b895704f6b21
Sinh Hà Đăng <hadangsinh1911@gmail.com>
2023-05-29 11:26:13 +0700

    add code

--- js/main.js
+++ js/main.js
@@ -5,14 +5,7 @@ window.onload = () => {
     navigator.serviceWorker
              .register('./sw.js');
   }
-  navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function(stream) {
-        // Camera access granted, stream contains the camera feed
-        console.log(stream);
-      })
-      .catch(function(error) {
-        // Camera access denied or an error occurred
-      });
+  
 }
 function test(){
   alert("hello");
