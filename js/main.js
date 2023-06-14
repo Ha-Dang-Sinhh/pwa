44e80a544d187b5b1f29d681ec34ecbb8d0603c9
Ha-Dang-Sinhh <hadangsinh1911@gmail.com>
2023-06-14 13:30:21 +0700

    add code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,49 @@
+// window.onload để đảm bảo rằng việc đăng ký Service Worker chỉ diễn ra
+// sau khi tất cả các tài nguyên trong trang web đã được tải hoàn thành.
 window.onload = () => {
   'use strict';
-
+  // Kiểm tra xem trình duyệt có hỗ trợ Service Worker hay không
   if ('serviceWorker' in navigator) {
+    // Đăng ký Service Worker với tệp tin sw.js
     navigator.serviceWorker
-             .register('./sw.js');
+             .register('./sw.js')
+               .then(function() { console.log('Service Worker Registered'); })
+               .catch(function(err) { console.log('Service Worker Failed to Register', err); });
   }
-  
 }
-function test(){
-  alert("hello");
-}
+// // Kiểm tra xem trình duyệt có hỗ trợ Prompt API hay không
+// if ('beforeinstallprompt' in window) {
+//   // Lắng nghe sự kiện beforeinstallprompt
+//   window.addEventListener('beforeinstallprompt', (event) => {
+//     // Ngăn chặn hiển thị cửa sổ cài đặt mặc định
+//     event.preventDefault();
+//     // Lưu event để sử dụng sau này
+//     deferredPrompt = event;
+//     // Hiển thị thông báo hoặc lời nhắc cài đặt ứng dụng tùy chỉnh
+//     // Ví dụ: hiển thị một nút "Cài đặt" để khuyến khích người dùng cài đặt ứng dụng
+//     showInstallPrompt();
+//   });
+// }
+//
+// // Hàm hiển thị thông báo hoặc lời nhắc cài đặt ứng dụng
+// function showInstallPrompt() {
+//   // Hiển thị thông báo hoặc lời nhắc cài đặt ứng dụng tùy chỉnh
+//   // Ví dụ: hiển thị một modal hoặc một snackbar chứa nút "Cài đặt"
+//   // Khi người dùng nhấp vào nút "Cài đặt", gọi hàm promptInstall()
+// }
+//
+// // Hàm gọi prompt để cài đặt ứng dụng
+// function promptInstall() {
+//   // Gọi prompt() trên deferredPrompt
+//   deferredPrompt.prompt();
+//   // Xử lý kết quả prompt
+//   deferredPrompt.userChoice.then((choiceResult) => {
+//     if (choiceResult.outcome === 'accepted') {
+//       console.log('Người dùng đã cài đặt ứng dụng');
+//     } else {
+//       console.log('Người dùng đã từ chối cài đặt ứng dụng');
+//     }
+//     // Xóa deferredPrompt sau khi đã sử dụng
+//     deferredPrompt = null;
+//   });
+// }
--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
+// Đây là tên của cache mà Service Worker sử dụng để lưu trữ các tệp tin được cache.
 let cacheName = 'hello-pwa';
+// Đây là danh sách các tệp tin mà bạn muốn cache trong ứng dụng.
 let filesToCache = [
   '/',
   '/index.html',
@@ -6,17 +8,17 @@ let filesToCache = [
   '/js/main.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
+  // để bỏ qua các quy trình cài đặt khác và kích hoạt ngay lập tức.
   self.skipWaiting();
 });
 
-/* Serve cached content when offline */
+
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
